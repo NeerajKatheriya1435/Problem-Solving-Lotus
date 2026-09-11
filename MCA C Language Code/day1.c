3ba24eae204139481b9a82529fa0b440472d4acb
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-09-11 14:08:30 +0530

    MCA C++ code

---
+++ MCA C Language Code/day1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("Hello kashish");
+    // int var1=14;
+    // int var2=8;
+// Arithmetic Operators
+    // printf("The sum is: %d\n",(var1+var2));
+    // printf("The sub is: %d\n",(var1-var2));
+    // printf("The mul is: %d\n",(var1*var2));
+    // printf("The div is: %.1f\n",((float) var1/var2));
+
+// Comparision Operator
+    // int var1=14;
+    // int var2=8;
+    // printf("%d\n",(var1>var2));  // 1
+    // printf("%d\n",(var1>=var2)); // 1
+    // printf("%d\n",(var1<=var2)); // 0
+    // printf("%d\n",(var1<var2)); // 0
+    // printf("%d\n",(var1==var2)); // 0 (14==8)
+    // printf("%d\n",(var1!=var2)); // 1  (14!=8)
+
+// C. Logical Operators
+
+    // int a=14;
+    // int b=8;
+    // printf("%d\n",((a>b) && (a>12) && (3<3))); // 1 0
+
+    // printf("%d\n",((a<b) || (a<12) || (3<3))); // 1 0
+    // printf("%d\n",((a>b) && (a>12) && (3<=3)));
+    // printf("%d\n",!(37>34));
+
+    // int a=5;
+    // a=a+3;
+    // a+=3;
+    // a-=2;
+    // a*=5;
+    // // printf("%d\n",a);
+    // int a=5;
+    // printf("%d\n",a); // 5
+    // printf("%d\n",a++); // 5
+    // printf("%d\n",++a); // 7
+    // printf("%d\n",a--); // 7
+    // printf("%d\n",a++); // 6
+    // printf("%d\n",a); // 7
+    // printf("%d\n",--a); // 6
+
+    // printf("%d\n",(5 & 7));
+
+    // int a=15;
+    // int b=8;
+    // printf("%x\n",&a);
+    // int var1= (a>b)? a:b; 
+    // printf("%d\n",(var1));
+
+    // int age; // declaration
+    // age=56; // inilization
+    // int a=67; // declaration with inilization
+
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+
+    printf("The value of age is: %d\n",age);
+
+
+
+
+
+
+    return 0;
+}
+
